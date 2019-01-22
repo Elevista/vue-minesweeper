ad3af474bb87c634d58ba456ca232ed5560babdc
Elevista <sunnyholic@sunnyholic.com>
2019-01-22 16:34:03 +0900

    package, webpack config update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,19 +1,19 @@
 const path = require('path')
 const webpack = require('webpack')
-module.exports = function (name, env = {}) {
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+module.exports = function (name) {
   return {
+    mode: 'development',
     name,
     module: {
       rules: [
-        {test: /\.js$/, include: path.resolve(__dirname, name), loader: 'vuejs-loader'},
-        {test: /\.mjs$/, loader: 'babel-loader'},
-        {test: /\.vue$/, include: path.resolve(__dirname, name), loader: 'vue-loader'},
-        {test: /\.css$/, loader: 'vue-style-loader!css-loader'},
-        {test: /\.scss$/, loader: 'vue-style-loader!css-loader!sass-loader'},
-        {test: /\.(png|woff|woff2|eot|ttf|svg|jpg|otf|gif)$/, loader: 'file-loader?outputPath=files/'}
+        { test: /\.js$/, include: path.resolve(__dirname, name), loader: 'vuejs-loader' },
+        { test: /\.vue$/, include: path.resolve(__dirname, name), loader: 'vue-loader' },
+        { test: /\.css$/, loader: 'vue-style-loader!css-loader' },
+        { test: /\.(png|woff|woff2|eot|ttf|svg|jpg|otf|gif)$/, loader: 'file-loader?outputPath=files/' }
       ]
     },
-    plugins: [new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery', _: 'lodash', Vue: 'vue'})],
-    resolve: {alias: {'~': path.resolve(__dirname, `${name}`)}}
+    plugins: [new VueLoaderPlugin(), new webpack.ProvidePlugin({ _: 'lodash', Vue: 'vue' })],
+    resolve: { alias: { '~': path.resolve(__dirname, `${name}`) } }
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path')
-const webpack = require('webpack')
 const baseConf = require('./webpack.config.base')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-module.exports = function (env = {}) {
+module.exports = function (env, argv) {
   let sourceMapFileNameTemplate = info => 'webpack:///' + info.resourcePath.replace(/\.vue$/, '.vue.html')
   let sourceMapFileNameDupTemplate = info => sourceMapFileNameTemplate(info) + info.query
 
-  let config = Object.assign(baseConf('app', env), {
+  let config = Object.assign(baseConf('app'), {
     entry: ['./app/boot.mjs'],
+    devtool: argv.mode === 'production' ? false : 'inline-source-map',
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'www/'),
@@ -16,12 +15,5 @@ module.exports = function (env = {}) {
       devtoolFallbackModuleFilenameTemplate: sourceMapFileNameDupTemplate
     }
   })
-  if (env.prod) {
-    process.env.NODE_ENV = 'production'
-    config.plugins.push(new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}), new UglifyJSPlugin())
-    config.entry.unshift('babel-polyfill')
-  } else {
-    config.devtool = 'inline-source-map'
-  }
   return config
 }
